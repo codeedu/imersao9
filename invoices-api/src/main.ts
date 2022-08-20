fa8adc36a37ffd0008dc45020dd208c91ace99f9
Luiz Carlos <argentinaluiz@gmail.com>
2022-08-20 11:32:33 -0300

    kafka connect e kubernetes

--- invoices-api/src/main.ts
+++ invoices-api/src/main.ts
@@ -16,6 +16,17 @@ async function bootstrap() {
       client: {
         clientId: process.env.KAFKA_CLIENT_ID,
         brokers: [process.env.KAFKA_HOST],
+        ssl: process.env.KAFKA_USE_SSL === 'true',
+              ...(process.env.KAFKA_SASL_USERNAME &&
+                process.env.KAFKA_SASL_USERNAME !== '' &&
+                process.env.KAFKA_SASL_PASSWORD &&
+                process.env.KAFKA_SASL_PASSWORD !== '' && {
+                  sasl: {
+                    mechanism: 'plain',
+                    username: process.env.KAFKA_SASL_USERNAME,
+                    password: process.env.KAFKA_SASL_PASSWORD,
+                  },
+                }),
       },
       consumer: {
         groupId:
